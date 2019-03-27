a46a187e4952e4b9b2e9f0aa395324abc596289d
healyma <healy.mark.a@gmail.com>
2019-03-27 22:40:07 +0000

    fix todo update

--- todos/update.js
+++ todos/update.js
@@ -14,13 +14,10 @@ export async function main(event, context) {
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET items = :items, attachments = :attachments, name=:name,notes =:notes,people= :people",
+    UpdateExpression: "SET listItems = :listItems, listName=:listName",
     ExpressionAttributeValues: {
-      ":items": data.items || null,
-      ":name" : data.name || null,
-      ":notes" : data.notes || null,
-      ":people" : data.people || null
-
+      ":listItems": data.items || null,
+      ":name" : datI.name || null
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
