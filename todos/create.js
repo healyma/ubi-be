c176eb3153ce7b4f34f1d4c6abe1f4c23a0044c8
healyma <healy.mark.a@gmail.com>
2019-04-07 00:52:26 +0100

    Moved todos to MqSQL

--- todos/create.js
+++ todos/create.js
@@ -4,7 +4,7 @@ import { success, failure } from "../libs/response-lib";
 export  function main(event, context) {
   try {
   const data = JSON.parse(event.body);
-  const qry="INSERT INTO List_LT (LT_Name, LY_Created_URID) VALUES ('" + data.LT_Name  + "','" + event.requestContext.identity.cognitoIdentityId + "'";
+  const qry="INSERT INTO List_LT (LT_Name, LY_Created_URID) VALUES ('" + data.LT_Name  + "','" + event.requestContext.identity.cognitoIdentityId + "')";
   database.query(qry , function (error, results) {
     if (error) {
         database.destroy();
