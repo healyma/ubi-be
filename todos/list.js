e2ed33e9c2749e84df06b85b6014a26ed5190028
healyma <healy.mark.a@gmail.com>
2019-04-07 17:58:53 +0100

    Moved todos to MqSQL

--- todos/list.js
+++ todos/list.js
@@ -1,7 +1,7 @@
 //import * as dynamoDbLib from "../libs/dynamodb-lib";
 import database from "../libs/database";
 import { success, failure } from "../libs/response-lib";
-export  function main(event, context) {
+export  function main(event, context,callback) {
   context.callbackWaitsForEmptyEventLoop = false;
   database.getConnection((err, connection) =>{
       connection.query("SELECT * FROM List_LT WHERE LT_Created_URID ='" + event.requestContext.identity.cognitoIdentityId + "'", function (error, results, fields) {
