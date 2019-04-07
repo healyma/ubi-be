2d61187916b905b076d72b67af52b9a5557ec127
healyma <healy.mark.a@gmail.com>
2019-04-07 17:55:47 +0100

    Moved todos to MqSQL

--- todos/create.js
+++ todos/create.js
@@ -8,7 +8,7 @@ export  function main(event, context,callback) {
   try{
   data = JSON.parse(event.body);
   }catch (e){
-    data={"body": { "LT_Name" : "Not defined"}}
+    data= { "LT_Name" : "Not defined"}
   }
   if(event.requestContext){
     userID=event.requestContext.identity.cognitoIdentityId;
@@ -18,7 +18,7 @@ export  function main(event, context,callback) {
   context.callbackWaitsForEmptyEventLoop = false;
   database.getConnection((err, connection) =>{
     // Use the connection
-    connection.query("INSERT INTO List_LT (LT_Name, LT_Created_URID) VALUES ('" + data.LT_Name  + "','" + userID + "')", function (error, results, fields) {
+    connection.query("INSERT INTO List_LT (LT_Name, LT_Created_URID) VALUES ('" + data.LT_Name  + "','" + userID + "')",  (error, results, fields) =>{
       connection.release();
       if (error) callback(error);
       else callback(null,results);
--- todos/list.js
+++ todos/list.js
@@ -6,8 +6,8 @@ export  function main(event, context) {
   database.getConnection((err, connection) =>{
       connection.query("SELECT * FROM List_LT WHERE LT_Created_URID ='" + event.requestContext.identity.cognitoIdentityId + "'", function (error, results, fields) {
         connection.release();
-        if (error) return failure( {status: false, error:error});
-         return success(results);
+        if (error) callback(error);
+        else callback(null,results);
       });
     });
 }
