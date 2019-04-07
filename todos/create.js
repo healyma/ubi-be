93323cce0be0c6b80a1ac9bb6d3ad6103d197fab
healyma <healy.mark.a@gmail.com>
2019-04-07 17:30:46 +0100

    Moved todos to MqSQL

--- todos/create.js
+++ todos/create.js
@@ -3,16 +3,25 @@ import { success, failure } from "../libs/response-lib";
 
 export  function main(event, context,callback) {
   console.log(event);
-  const data = event.body;
-
+  var data;
+  var userID;
+  try{
+  data = JSON.parse(event.body);
+  }catch (e){
+    data={"body": { "LT_Name" : "Not defined"}}
+  }
+  if(event.requestContext){
+    userID=event.requestContext.identity.cognitoIdentityId;
+  }else{
+    userID="";
+  }
   context.callbackWaitsForEmptyEventLoop = false;
   database.getConnection((err, connection) =>{
     // Use the connection
-    connection.query("INSERT INTO List_LT (LT_Name, LT_Created_URID) VALUES ('" + data.LT_Name  + "','" + event.requestContext.identity.cognitoIdentityId + "')", function (error, results, fields) {
+    connection.query("INSERT INTO List_LT (LT_Name, LT_Created_URID) VALUES ('" + data.LT_Name  + "','" + userID + "')", function (error, results, fields) {
       connection.release();
-      if (err) return failure( {status: false, error:err});
-      if(error) return failure( {status: false, error:error});
-      return success(results);
+      if (error) callback(error);
+      else callback(null,results);
     });
   });
 }
