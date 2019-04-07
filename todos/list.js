af8799c84c2c1af9fcf311146c335ff739c9d8b2
healyma <healy.mark.a@gmail.com>
2019-04-07 02:00:25 +0100

    Moved todos to MqSQL

--- todos/list.js
+++ todos/list.js
@@ -5,8 +5,8 @@ export  function main(event, context) {
   database.getConnection((err, connection) =>{
       connection.query("SELECT * FROM List_LT WHERE LT_Created_URID ='" + event.requestContext.identity.cognitoIdentityId + "'", function (error, results, fields) {
         connection.release();
-        if (error) callback(error);
-        else callback(null,results);
+        if (error) return failure( {status: false, error:error});
+        else return success(results);
       });
     });
 }
